2bad9447077c0b89e8ca0ceaa641f1cf8350474f
AndriusK <akrisiun@gmail.com>
2017-01-15 13:27:07 +0200

    restore .cmd update

--- nuget-init.cmd
+++ nuget-init.cmd
@@ -1,1 +1,5 @@
+nuget install Microsoft.Web.Infrastructure   -outputDirectory Packages
 nuget install Newtonsoft.Json -version 9.0.1 -outputDirectory Packages
+
+copy packages\Microsoft.Web.Infrastructure.1.0.0.0\lib\net40\Microsoft.Web.Infrastructure.dll lib\*.* 
+copy packages\Newtonsoft.Json.9.0.1\lib\net45\Newtonsoft.Json.dll  lib\*.* 
