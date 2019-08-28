2d0a41be051f346d9e5c0ae811310de2bf9c7c5a
AndriusK <andrius.k@sanitex.eu>
2019-08-28 22:11:27 +0300

    pack with dotnet, 5.2.7-beta1, with DelaySign

--- b.ps1
+++ b.ps1
@@ -1,5 +1,10 @@
 
 dotnet restore
 
+.\build.cmd EnableSkipStrongNames
+
+# %MSBuild% 
+msbuild16 Runtime.msbuild /m /nr:false /p:Platform="Any CPU" /p:Desktop=true /v:M /fl 
+
 # dotnet build -o $PWD\lib\ --no-restore -f net461
-dotnet build -o $PWD\lib\ --no-restore
+dotnet build -o $PWD\lib\ --no-restore   -f net471 
--- pack.ps1
+++ pack.ps1
@@ -1,14 +1,21 @@
 
 dotnet sln list
-remove-item *.nupkg -force
+# remove-item *.nupkg -force
 
-nuget pack src\System.Web.Http\System.Web.Http.csproj
+dotnet pack src\System.Net.Http.Formatting\System.Net.Http.Formatting.csproj  -o $PWD
+dotnet pack src\System.Web.Http\System.Web.Http.csproj                        -o $PWD
+dotnet pack src\System.Web.Http.WebHost\System.Web.Http.WebHost.csproj        -o $PWD
+
+dotnet build src\System.Net.Http.Formatting\System.Net.Http.Formatting.csproj  -o $PWD/lib/ -f net471
+dotnet build src\System.Web.Http.WebHost\System.Web.Http.WebHost.csproj        -o $PWD/lib/ -f net471
+dotnet build src\System.Web.Http\System.Web.Http.csproj                        -o $PWD/lib/ -f net471
+
+exit;
+
 nuget pack src\System.Web.Mvc\System.Web.Mvc.csproj
-nuget pack src\System.Web.Http.WebHost\System.Web.Http.WebHost.csproj
 
 nuget pack src\System.Web.Helpers\System.Web.Helpers.csproj
 nuget pack src\System.Web.Razor\System.Web.Razor.csproj
-nuget pack src\System.Net.Http.Formatting\System.Net.Http.Formatting.csproj
 nuget pack src\System.Web.WebPages\System.Web.WebPages.csproj
 nuget pack src\System.Web.WebPages.Deployment\System.Web.WebPages.Deployment.csproj
 nuget pack src\System.Web.WebPages.Razor\System.Web.WebPages.Razor.csproj
