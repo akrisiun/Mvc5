fd92da6cf51e0365b680772a2d025576f86a8afd
AndriusK <akrisiun@gmail.com>
2019-08-30 22:05:55 +0300

    up recompile

---
+++ lib/gac57.ps1
@@ -0,0 +1,7 @@
+
+& "c:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.7.1 Tools\gacutil.exe"  /i System.Web.Mvc.dll
+& "c:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.7.1 Tools\gacutil.exe"  /i System.Web.Razor.dll
+& "c:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.7.1 Tools\gacutil.exe"  /i System.Web.WebPages.dll
+& "c:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.7.1 Tools\gacutil.exe"  /i System.Web.WebPages.Razor.dll
+
+
